a3c9ad7553f777d6180ee72bfe84e9dae1a0d067
karim-8 <kimo_105@hotmail.com>
2016-11-26 18:56:56 +0200

    mm

--- app/src/main/java/com/example/karim/youtubeapiexample/MainActivity.java
+++ app/src/main/java/com/example/karim/youtubeapiexample/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
 
     YouTubePlayerView youTubePlayerView;
     //Api key
-    public static final String Api_Key = "";
+    public static final String Api_Key = "bbsb";
 
     //video id
     public static final String Video_Id1 = "-m3V8w_7vhk";
